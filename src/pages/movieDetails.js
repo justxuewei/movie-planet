537bcecbfadd5a938932e18c7769a123cfb537c6
Xuewei Niu <justxuewei@apache.org>
2022-04-19 21:49:04 +0800

    feat: init movie details

---
+++ src/pages/movieDetails.js
@@ -0,0 +1,17 @@
+let movies = {
+    'title': {
+        images: ['image-urls'],
+        actors: ['actors1'],
+        description: '',
+        type: '',
+        ratings: '',
+        date: '',
+    }
+}
+
+let actors = {
+    'actor1': {
+        image: '',
+        description: '',
+    }
+}
